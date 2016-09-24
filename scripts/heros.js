07445654c311cc7e35036bc2948a31b8e8670d09
mukesh <kothiyal.mukesh@gmail.com>
2016-09-24 13:11:21 +0530

    initial code changes for all heros

---
+++ scripts/heros.js
@@ -0,0 +1,90 @@
+function HeroListController($scope, $element, $attrs) {
+    var ctrl = this;
+
+    // This would be loaded by $http etc.
+    ctrl.list = [
+        {
+            name: 'Superman',
+            location: ''
+        },
+        {
+            name: 'Batman',
+            location: 'Wayne Manor'
+        }
+    ];
+
+    ctrl.updateHero = function (hero, prop, value) {
+        hero[prop] = value;
+    };
+
+    ctrl.deleteHero = function (hero) {
+        var idx = ctrl.list.indexOf(hero);
+        if (idx >= 0) {
+            ctrl.list.splice(idx, 1);
+        }
+    };
+}
+
+function HeroDetailController() {
+    var ctrl = this;
+
+    ctrl.delete = function () {
+        ctrl.onDelete({hero: ctrl.hero});
+    }
+
+    ctrl.update = function (prop, value) {
+        ctrl.onUpdate({hero: ctrl.hero, prop: prop, value: value});
+    }
+}
+
+function EditableFieldController() {
+    var ctrl = this;
+    ctrl.editMode = false;
+
+    ctrl.handleModeChange = function () {
+        if (ctrl.editMode) {
+            ctrl.onUpdate({value: ctrl.fieldValue});
+            ctrl.fieldValueCopy = ctrl.fieldValue;
+        }
+        ctrl.editMode = !ctrl.editMode;
+    };
+
+    ctrl.reset = function () {
+        ctrl.fieldValue = ctrl.fieldValueCopy;
+    };
+
+    ctrl.$onInit = function () {
+        // Make a copy of the initial value to be able to reset it later
+        ctrl.fieldValueCopy = ctrl.fieldValue;
+
+        // Set a default fieldType
+        if (!ctrl.fieldType) {
+            ctrl.fieldType = 'text';
+        }
+    };
+}
+
+angular.module('my-heros', [])
+        .component('heroList', {
+            templateUrl: '../my_components/hero-component/views/heroList.html',
+            controller: HeroListController
+        })
+        .component('heroDetail', {
+            templateUrl: '../my_components/hero-component/views/heroDetails.html',
+            controller: HeroDetailController,
+            bindings: {
+                hero: '<',
+                onDelete: '&',
+                onUpdate: '&'
+            }
+        })
+        .component('editableField', {
+            templateUrl: '../my_components/hero-component/views/editableField.html',
+            controller: EditableFieldController,
+            bindings: {
+                fieldValue: '<',
+                fieldType: '@?',
+                onUpdate: '&'
+            }
+        })
+;
